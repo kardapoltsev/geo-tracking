e6874d5f6fc069f2e3bd60c94bffd54fa6c985a9
Alexey Kardapoltsev <alexey.kardapoltsev@gmail.com>
2016-09-04 21:02:50 +0500

    gzip compressions for requests

--- build.sbt
+++ build.sbt
@@ -15,15 +15,16 @@ proguardOptions in Android ++= Seq(
   "-keepattributes Signature",
   "-printseeds target/seeds.txt",
   "-printusage target/usage.txt",
+  "-dontwarn okio.**",
   "-dontwarn scala.collection.**", // required from Scala 2.11.4
   "-dontwarn org.scaloid.**" // this can be omitted if current Android Build target is android-16
 )
 
 libraryDependencies ++= Seq(
-  "org.scaloid"         %% "scaloid"            % "4.2",
-  "com.android.support" %  "support-core-utils" % "24.2.0",
-  "com.loopj.android"   %  "android-async-http" % "1.4.9",
-  "io.spray" %%  "spray-json" % "1.3.2"
+  "org.scaloid"          %% "scaloid"            % "4.2",
+  "com.android.support"  %  "support-core-utils" % "24.2.0",
+  "com.squareup.okhttp3" %  "okhttp"             % "3.4.1",
+  "io.spray"             %% "spray-json"         % "1.3.2"
 )
 
 run <<= run in Android
--- src/main/scala/com/github/kardapoltsev/geotracking/api/Api.scala
+++ src/main/scala/com/github/kardapoltsev/geotracking/api/Api.scala
@@ -1,46 +1,51 @@
 package com.github.kardapoltsev.geotracking.api
 
 
+import java.io.{ByteArrayOutputStream, IOException}
+import java.util.zip.GZIPOutputStream
+
 import android.content.Context
 import android.util.Log
-import com.loopj.android.http.{AsyncHttpClient, AsyncHttpResponseHandler}
-import cz.msebera.android.httpclient.Header
-import cz.msebera.android.httpclient.entity.StringEntity
+import okhttp3._
 import spray.json._
 
 
 
 object Api {
-  private lazy val client = new AsyncHttpClient()
+  private lazy val client = new OkHttpClient()
   private val ApiUrl = ""
   private val SendLocationUrl = ApiUrl + "geo/location"
+  val jsonMediaType = MediaType.parse("application/json; charset=utf-8")
 
   def sendLocation(locations: Seq[Location])(implicit ctx: Context): Unit = {
     if(locations.nonEmpty) {
       //Log.d("API", s"sending ${locations.toJson.prettyPrint}")
-      val entity = new StringEntity(locations.toJson.compactPrint)
-      client.post(ctx, SendLocationUrl, entity, "application/json", sendLocationResponseHandler)
+      val body = RequestBody.create(jsonMediaType, gzip(locations))
+      val request = new Request.Builder().
+        url(SendLocationUrl).
+        header("Content-Encoding", "gzip").
+        post(body).
+        build()
+
+      client.newCall(request).enqueue(new Callback(){
+        override def onFailure(call: Call, e: IOException): Unit = {
+          Log.e("API", "couldn't send location", e)
+        }
+
+        override def onResponse(call: Call, response: Response): Unit = {
+          Log.i("API", s"locations were sent: $response")
+        }
+      })
     }
   }
 
-  private def sendLocationResponseHandler =
-    new AsyncHttpResponseHandler() {
-      override def onFailure(
-        statusCode: Int,
-        headers: Array[Header],
-        responseBody: Array[Byte],
-        error: Throwable): Unit = {
-        Log.e("API", "couldn't send location. response status: " + statusCode, error)
-      }
-
-      override def onSuccess(
-        statusCode: Int,
-        headers: Array[Header],
-        responseBody: Array[Byte]
-      ): Unit = {
-        Log.i("API", "locations were sent")
-      }
-    }
-
+  private def gzip(locations: Seq[Location]): Array[Byte] = {
+    val data = locations.toJson.compactPrint.getBytes("UTF-8")
+    val arr = new ByteArrayOutputStream()
+    val zipper = new GZIPOutputStream(arr)
+    zipper.write(data)
+    zipper.close()
+    arr.toByteArray
+  }
 
 }
